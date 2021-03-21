97d35be52ea253190983b081863358f40d2eebcb
Matheus Pires <matheus.pires@tech4h.com.br>
2021-03-21 15:00:26 -0300

    init: initialization strapi &  auth configuration

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '093b1881cb1533ee3d89355062b29c50'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12577132-688a-4629-be94-c15d145c48f3'
+};
